9d4931e36086b19c93f930bc374b72f75789f828
Dongu-rami <kdg4842@hanyang.ac.kr>
2021-05-31 23:20:44 +0900

    체크박스 업데이트

--- run.py
+++ run.py
@@ -1,6 +1,54 @@
 from process import run
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
-run(False,show=False,save_fig=True,save_csv=True)
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.cb1 = QCheckBox('Show figure', self)
+        self.cb2 = QCheckBox('Save figure', self)
+        self.cb3 = QCheckBox('Save csv', self)
+        self.cb1.move(20, 20)
+        self.cb2.move(20, 60)
+        self.cb3.move(20, 100)
+        self.cb1.toggle()
+        self.cb2.toggle()
+        self.cb3.toggle()
+
+        self.btnSave = QPushButton("set", self)
+        self.btnSave.move(10, 160)
+        self.btnSave.clicked.connect(self.btnInput_clicked)
+        self.btnSave.clicked.connect(QCoreApplication.instance().quit)
+
+        self.setWindowTitle('QCheckBox')
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+    def btnInput_clicked(self):
+        global show
+        global save_fig
+        global save_csv
+        show = self.cb1.isChecked()
+        save_fig = self.cb1.isChecked()
+        save_csv = self.cb1.isChecked()
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    app.exec_()
+    print(show)
+    print(save_fig)
+
+
+run('.\data\**\*.xml',show ,save_fig,save_csv)
 
 # run(path_file,show=False,save_fig=False,save_csv=False,file_input=None)
 
