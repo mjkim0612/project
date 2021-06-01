f1fdfdcc0274b8b105f316bffbc8c3686eb3d08f
atom0605 <maple980605@naver.com>
2021-06-01 22:29:15 +0900

    report 파일 code structure 작성

--- src/data_set.py
+++ src/data_set.py
@@ -2,11 +2,11 @@ def data_set(data,ab=0):
     # ab가 1이면 절대값
     value = []
     for i in range(0,len(data)):
-        y_value1 = data[i].text.split(',')
-        y_value1 = list(map(float,y_value1))
+        value1 = data[i].text.split(',')
+        value1 = list(map(float,value1))
         if ab == 0:
-            value.append(y_value1)
+            value.append(value1)
         elif ab == 1:
-            value2 = list(map(abs,y_value1))
+            value2 = list(map(abs,value1))
             value.append(value2)
     return value
